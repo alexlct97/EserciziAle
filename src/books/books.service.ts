05d02651bcb2a1988c4bf67b9409d881d085ac0c
alexlct97 <alexlct@virgilio.it>
2026-05-11 13:46:42 +0200

    test done

---
+++ src/books/books.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+@Injectable()
+export class BooksService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createBookDto: CreateBookDto) {
+    return this.prisma.book.create({
+      data: createBookDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.book.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.book.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return this.prisma.book.update({
+      where: { id },
+      data: updateBookDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.book.delete({
+      where: { id },
+    });
+  }
+}
