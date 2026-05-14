d8b68538ad52a346419a665ee0142d74cd5d4c75
alexlct97 <alexlct@virgilio.it>
2026-05-14 09:08:50 +0200

    add swagger setup

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,14 +1,27 @@
 import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBookDto {
+  @ApiProperty({
+    description: 'The title of the book',
+    example: 'The Great Gatsby',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    description: 'The author of the book',
+    example: 'F. Scott Fitzgerald',
+  })
   @IsString()
   @IsNotEmpty()
   author: string;
 
+  @ApiProperty({
+    description: 'The year the book was published',
+    example: 1925,
+  })
   @IsNumber()
   year: number;
 }
